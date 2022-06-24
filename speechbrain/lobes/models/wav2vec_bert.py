1456f83b4d430dc28e682af270934bb4ad85e7bc
cmdoret <cyril.mattheydoret@gmail.com>
2022-06-24 18:29:54 +0200

    dummy-commit

--- speechbrain/lobes/models/wav2vec_bert.py
+++ speechbrain/lobes/models/wav2vec_bert.py
@@ -822,6 +822,7 @@ class TransformerEncoder(nn.Module):
 class ConformerEncoder(TransformerEncoder):
     def build_encoder_layer(self, args):
         ...
+        layer = None
         # layer = ConformerWav2Vec2EncoderLayer(
         #     embed_dim=self.embedding_dim,
         #     ffn_embed_dim=args.encoder_ffn_embed_dim,
